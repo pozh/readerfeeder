1129a5a933cfca2e7d39abc2595006e5707401ce
Sergey Pozhilov <pozhilov@gmail.com>
2018-02-15 23:31:17 +0300

    Homepage refactoring: Hero component deleted

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,7 +7,27 @@ import { Header, Footer } from './parts/Layout';
 export const HomePage = () => (
   <div>
     <Header />
-    <Hero />
+
+    <section className="Hero bg-light">
+      <Container>
+        <Row>
+          <Col md="7">
+            <h1 className="Hero-heading">RSS Delivery Service for KINDLE</h1>
+            <h4 className="Hero-subheading">
+              ReaderFeeder delivers RSS feeds in full-text format to your
+              Kindle e-reader, wirelessly, on schedule.
+            </h4>
+            <div className="Hero-actions">
+              <Link className="btn btn-lg btn-round btn-primary" to="/browse">Selected Feeds</Link>
+            </div>
+          </Col>
+          <Col md="5">
+            <img src={require("assets/images/news.png")} alt="" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+
 
     <section className="Intro">
       <Container>
@@ -57,29 +77,6 @@ export const HomePage = () => (
 
     <Footer />
   </div>
-);
-
-
-const Hero = () => (
-  <section className="Hero bg-light">
-    <Container>
-      <Row>
-        <Col md="7">
-          <h1 className="Hero-heading">RSS Delivery Service for KINDLE</h1>
-          <h4 className="Hero-subheading">
-            ReaderFeeder delivers RSS feeds in full-text format to your
-            Kindle e-reader, wirelessly, on schedule.
-          </h4>
-          <div className="Hero-actions">
-            <Link className="btn btn-lg btn-round btn-primary" to="/browse">See Selected Feeds</Link>
-          </div>
-        </Col>
-        <Col md="5">
-          <img src={require("assets/images/news.png")} alt="" />
-        </Col>
-      </Row>
-    </Container>
-  </section>
 );
 
 export default HomePage;
