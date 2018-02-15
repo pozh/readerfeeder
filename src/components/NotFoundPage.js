0554acd23b121db8b5c8e725d65d650f7dfa2aa0
Sergey Pozhilov <pozhilov@gmail.com>
2018-02-15 23:30:31 +0300

    NotFound page revised

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {Header} from 'components/parts/Header';
-import {Footer} from 'components/parts/Footer';
+import {Header, PageCaption, Footer} from './parts/Layout';
+import { Container, Row, Col } from 'reactstrap';
 
-export const NotFoundPage = props => (
+
+const NotFoundPage = props => (
   <div>
-    <Header/>
-    <section className="u-h-100vh u-flex-center">
-      <div className="container">
-        <div className="row">
-          <div className="col-12 text-center">
-            <h1 className="u-fs-60 u-fs-md-150 ">404</h1>
-            <h2 className="mb-4">Sorry! Page Not Found</h2>
-            <p>The link you have followed is broken, or the page has been removed.</p>
-            <Link to="/" className="btn btn-rounded btn-primary mt-5">Return to Home</Link>
-          </div>
-        </div>
-      </div>
-    </section>
+    <Header className="white" />
+    <PageCaption caption="Not Found" extra="" />
+
+    <Container className="pt-5">
+      <h1>404</h1>
+      <h2 className="mb-4">Sorry! Page Not Found</h2>
+      <p>The link you have followed is broken, or the page has been removed.</p>
+      <Link to="/" className="btn btn-round btn-primary mt-5">Return to Home</Link>
+    </Container>
+
     <Footer/>
   </div>
 );
+
+export default NotFoundPage;
