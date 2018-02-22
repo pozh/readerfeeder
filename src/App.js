42bbfc155d5249a87de9fdf781de0f9493347a1a
Sergey Pozhilov <pozhilov@gmail.com>
2018-02-22 10:57:56 +0300

    Moved root stuff from index to App.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HomePage } from './components/HomePage';
+import { AboutPage } from './components/AboutPage';
+import { FeedPage } from './components/FeedPage';
+import { BrowsePage } from './components/BrowsePage';
+import { NotFoundPage } from "./components/NotFoundPage";
+import Storage from "./utils/Storage";
+
+
+export class App extends Component {
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={HomePage}/>
+          <Route exact path="/about" component={AboutPage}/>
+          <Route exact path="/feeds" component={BrowsePage}/>
+          <Route path="/feeds/:slug" component={NotFoundPage}/>
+          <Route path="/feed/:slug" component={FeedPage}/>
+        </div>
+      </Router>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { HomePage } from './components/HomePage';
-import { AboutPage } from './components/AboutPage';
-import { FeedPage } from './components/FeedPage';
-import { BrowsePage } from './components/BrowsePage';
-import { NotFoundPage } from "./components/NotFoundPage";
-
+import { App } from './App';
 import 'assets/styles/main.scss';
 
-render((
-  <Router>
-    <div>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/about" component={AboutPage} />
-      <Route exact path="/feeds" component={BrowsePage} />
-      <Route path="/feeds/:slug" component={NotFoundPage} />
-      <Route path="/feed/:slug" component={FeedPage} />
-    </div>
-  </Router>
-), document.getElementById('app'));
+render(
+  <App />,
+  document.getElementById('app')
+);
