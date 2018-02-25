fd331806fc36481605500e854bb10aa724e4d145
Sergey Pozhilov <pozhilov@gmail.com>
2018-02-25 23:38:39 +0300

    Register page added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { HomePage } from './scenes/Home';
-import { LoginPage } from './scenes/Login';
+import { LoginPage } from './scenes/Auth/Login';
+import { SignupPage } from './scenes/Auth/Signup';
 import { FeedInfo } from './scenes/FeedInfo';
 import { FeedsPage } from './scenes/Feeds';
 // import { NotFoundPage } from "./scenes/NotFound";
@@ -15,6 +16,7 @@ export class App extends Component {
         <div>
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/login" component={LoginPage}/>
+          <Route exact path="/signup" component={SignupPage}/>
           <Route exact path="/feeds" component={FeedsPage}/>
           <Route path="/feeds/:order(popular|recent|my)" component={FeedsPage}/>
           <Route path="/feeds/category/:category" component={FeedsPage}/>
@@ -23,4 +25,17 @@ export class App extends Component {
       </Router>
     );
   }
+
+
+  requireAuth(nextState, replace) {
+    const token = window.sessionStorage.token;
+    if (!token) {
+      replace('/login');
+    }
+  }
+
+  logout(nextState, replace) {
+    delete window.sessionStorage.token;
+  }
+
 }
--- src/scenes/Auth/Signup/index.js
+++ src/scenes/Auth/Signup/index.js
@@ -4,42 +4,37 @@ import { Form, FormGroup, Label, Input } from 'reactstrap';
 import './styles.scss';
 
 
-export const LoginPage = props => {
+export const SignupPage = props => {
   return (
-    <section className="Login">
-      <div className="Login-wrapper align-self-center">
+    <section className="Signup">
+      <div className="Signup-wrapper align-self-center">
 
         <p className="text-center">
-          <Link className="Login-logo" to="/"><img src={require("./../../assets/images/logo.png")} alt="" /></Link>
+          <Link className="Signup-logo" to="/"><img src={require("assets/images/logo.png")} alt="" /></Link>
         </p>
 
         <div className="card card-dialog mx-auto mt-4 Login-dialog">
           <Form className="card-body">
 
-            <h3 className="h5 text-center mt-3">Log in to ReaderFeeder</h3>
+            <h3 className="h5 text-center mt-3">Create Account</h3>
             <p className="text-center">Enter your email address and password below.</p>
 
             <hr className="mt-4"/>
 
             <FormGroup><Input type="email" name="email" placeholder="Email" /></FormGroup>
             <FormGroup><Input type="password" name="password" placeholder="Password" /></FormGroup>
-            <FormGroup check className="mb-4">
-              <Label check>
-                <Input type="checkbox" />{' '}
-                Remember me
-              </Label>
-            </FormGroup>
+            <FormGroup><Input type="password" name="password2" placeholder="Repeat Password" /></FormGroup>
 
             <div className="row m-t-20">
               <div className="col text-right">
-                <a href="/app.html" className="btn btn-round btn-primary">LOGIN</a>
+                <a href="/app.html" className="btn btn-round btn-primary">REGISTER</a>
               </div>
               <div className="col text-right hidden">
                 <button type="button" className="btn btn-circle btn-facebook">
-                  <img src={require('./../../assets/images/ico_facebook.png')} alt=""/>
+                  <img src={require('assets/images/ico_facebook.png')} alt=""/>
                 </button>
                 <button type="button" className="btn btn-circle btn-google">
-                  <img src={require('./../../assets/images/ico_google.png')} alt=""/>
+                  <img src={require('assets/images/ico_google.png')} alt=""/>
                 </button>
               </div>
             </div>
@@ -48,9 +43,7 @@ export const LoginPage = props => {
         </div>
 
         <p className="mt-4 text-center">
-          <Link to="/reset-password" className="">Forgot Password?</Link>
-          <br/>
-            <strong>Don’t have an account? <Link to="/signup" className="">Register Now</Link></strong>
+            Already have an account? <strong><Link to="/login" className="">Login here</Link></strong>
         </p>
 
       </div>
