8cf3ef24289356a5a43fb5369ce9d2a99061dad8
Sergey Pozhilov <pozhilov@gmail.com>
2018-02-27 17:08:35 +0300

    Login page restyled, including body

--- src/scenes/Auth/Login/index.js
+++ src/scenes/Auth/Login/index.js
@@ -1,38 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Form, FormGroup, Label, Input } from 'reactstrap';
+import {NotificationContainer, NotificationManager} from 'react-notifications';
 import './styles.scss';
+import {apiEndPoints as api} from "../../../constants";
 
 
-export const LoginPage = props => {
-  return (
-    <section className="Login">
-      <div className="Login-wrapper align-self-center">
+export class LoginPage extends Component {
 
-        <p className="text-center">
-          <Link className="Login-logo" to="/"><img src={require("assets/images/logo.png")} alt="" /></Link>
-        </p>
+  constructor(props) {
+    super(props);
 
-        <div className="card card-dialog mx-auto mt-4 Login-dialog">
-          <Form className="card-body">
+    this.state = {
+      errors: {},
+      user: {
+        email: '',
+        password: ''
+      }
+    };
 
-            <h3 className="h5 text-center mt-3">Log in to ReaderFeeder</h3>
-            <p className="text-center">Enter your email address and password below.</p>
+    this.processForm = this.processForm.bind(this);
+    this.changeUser = this.changeUser.bind(this);
+  }
 
-            <hr className="mt-4"/>
+  processForm(event) {
+    event.preventDefault();
+    NotificationManager.info('Info message');
+    console.log('name:', this.state.user.name);
+    console.log('email:', this.state.user.email);
+    console.log('password:', this.state.user.password);
+  }
 
-            <FormGroup><Input type="email" name="email" placeholder="Email" /></FormGroup>
-            <FormGroup><Input type="password" name="password" placeholder="Password" /></FormGroup>
-            <FormGroup check className="mb-4">
+  changeUser(event) {
+    const field = event.target.name;
+    const user = this.state.user;
+    user[field] = event.target.value;
+    this.setState({user});
+  }
+
+  componentWillMount(){
+    document.getElementById('body').className='BodyLogin';
+  }
+
+  componentWillUnmount(){
+    let elems = document.querySelectorAll(".BodyLogin");
+    [].forEach.call(elems, (el) => { el.classList.remove("BodyLogin"); });
+  }
+
+  render () {
+    return (
+      <section className="Login">
+        <div className="Login-dialog">
+          <Form className="Login-form" onSubmit={this.processForm} action={api.login}>
+
+            <Link className="Login-logo" to="/"><img src={require("assets/images/logo.png")} alt=""/></Link>
+            <p className="Login-greeting">Welcome back!</p>
+            <p className="Login-cta">Sign in to continue to ReaderFeeder.</p>
+
+            <FormGroup><Input type="email" onChange={this.changeUser} name="email" placeholder="Email"/></FormGroup>
+            <FormGroup><Input type="password" onChange={this.changeUser} name="password" placeholder="Password"/></FormGroup>
+            <FormGroup check className="mb-4 hidden">
               <Label check>
-                <Input type="checkbox" />{' '}
+                <Input type="checkbox"/>{' '}
                 Remember me
               </Label>
             </FormGroup>
 
             <div className="row m-t-20">
-              <div className="col text-right">
-                <a href="/app.html" className="btn btn-round btn-primary">LOGIN</a>
+              <div className="col">
+                <button type="submit" className="btn btn-primary">LOGIN</button>
               </div>
               <div className="col text-right hidden">
                 <button type="button" className="btn btn-circle btn-facebook">
@@ -44,16 +80,17 @@ export const LoginPage = props => {
               </div>
             </div>
 
+            <p className="mt-4">
+              {/*<strong><Link to="/reset-password" className="">Forgot Password?</Link></strong>*/}
+              {/*<br/>*/}
+              Don’t have an account? <strong><Link to="/signup" className="">Sign Up</Link></strong>
+            </p>
           </Form>
         </div>
-
-        <p className="mt-4 text-center">
-          <strong><Link to="/reset-password" className="">Forgot Password?</Link></strong>
-          <br/>
-            Don’t have an account? <strong><Link to="/signup" className="">Register Now</Link></strong>
-        </p>
-
-      </div>
-    </section>
-  );
+        <NotificationContainer/>
+      </section>
+    );
+  }
 }
+
+export default LoginPage;
