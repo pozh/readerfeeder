4eda0e9b331c070494a203f21b12072e814d724e
Sergey Pozhilov <pozhilov@gmail.com>
2018-02-28 22:41:11 +0300

    Dynamic header/navbar - for logged in user and for guests

--- src/scenes/components/Header.js
+++ src/scenes/components/Header.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { Container, Navbar, NavbarToggler, Nav, NavItem, Collapse } from 'reactstrap';
+import Auth from 'utils/Auth';
 
 
-export class Header extends React.Component {
+export default class Header extends React.Component {
   constructor(props) {
     super(props);
     this.toggle = this.toggle.bind(this);
+    this.logout = this.logout.bind(this);
     this.state = {
-      isOpen: false
+      isOpen: false,
     };
   }
+
   toggle() {
     this.setState({
       isOpen: !this.state.isOpen
     });
   }
+
+  logout(event) {
+    event.preventDefault();
+    Auth.deauthenticate();
+    window.location.reload();
+  }
+
   render() {
     const isLight = this.props.light;
     return (
@@ -24,19 +34,30 @@ export class Header extends React.Component {
           <Link className="navbar-brand" to="/"><img src={require("assets/images/logo.png")} alt="" /></Link>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <Link className="btn btn-link" to="/login/">Log In</Link>
-              </NavItem>
-              <NavItem>
-                <Link className="btn btn-round btn-primary" to="/signup/">Sign Up</Link>
-              </NavItem>
-            </Nav>
+            {!Auth.isAuthenticated() && (
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Link className="btn btn-link" to="/login/">Log In</Link>
+                </NavItem>
+                <NavItem>
+                  <Link className="btn btn-round btn-primary" to="/signup/">Sign Up</Link>
+                </NavItem>
+              </Nav>
+            )}
+            {Auth.isAuthenticated() && (
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <Link className="btn btn-round btn-primary" to="/user">Dashboard</Link>
+                </NavItem>
+                {/*<NavItem>*/}
+                  {/*<Link className="btn btn-link" onClick={this.logout} to="#">Log Out</Link>*/}
+                {/*</NavItem>*/}
+              </Nav>
+            )}
+
           </Collapse>
         </Container>
       </Navbar>
     );
   }
 }
-
-export default Header;
