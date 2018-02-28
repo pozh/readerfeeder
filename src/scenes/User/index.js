d8a4a49888796ab3ece5bc0e0c4a328a68de2716
Sergey Pozhilov <pozhilov@gmail.com>
2018-02-28 17:04:55 +0300

    Auth checking in user page

--- src/scenes/User/index.js
+++ src/scenes/User/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { Footer } from './../components/Footer';
 import { Header } from './../components/Header';
 import { PageCaption } from './../components/PageCaption';
-import AppState from "../../utils/AppState";
+import Auth from 'utils/Auth';
 import './styles.scss';
 
 export const UserPage = props => {
-  return (
+  if (!Auth.isAuthenticated()) return <Redirect to='/login'/>;
+  else return (
     <div>
       <Header className="white" />
       <PageCaption>
-        Browse Feeds
-        {categorySlug && ' → ' + AppState.categoryBySlug(categorySlug).title}
+        User
       </PageCaption>
       <div className="container">
         <h1>User home page</h1>
