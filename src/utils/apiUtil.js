2c0798a9ed546134bdf8fbc0bb432bffcea235f6
Sergey Pozhilov <pozhilov@gmail.com>
2018-03-14 11:06:36 +0300

    Cookies read fixed in apiUtil

--- src/utils/apiUtil.js
+++ src/utils/apiUtil.js
@@ -1,26 +1,33 @@
 import axios from 'axios';
-import cookie from 'react-cookie';
+import Cookies from 'universal-cookie';
+
+const cookieLoad = cookieName => {
+  const cookies = new Cookies();
+  return cookies.get(cookieName);
+};
+
 
 export function fetch(url, pathParam) {
+
   return axios.get(url + pathParam, {
-    headers: {'Authorization': 'Bearer' + ' ' + cookie.load('token')}
+    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad('token')}
   });
 }
 
 export function store(url, pathParam, data) {
   return axios.post(url + pathParam, data, {
-    headers: {'Authorization': 'Bearer' + ' ' + cookie.load('token')}
+    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad('token')}
   });
 }
 
 export function update(url, pathParam, data) {
   return axios.put(url + pathParam, data, {
-    headers: {'Authorization': 'Bearer' + ' ' + cookie.load('token')}
+    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad('token')}
   });
 }
 
 export function destroy(url, pathParam) {
   return axios.delete(url + pathParam, {
-    headers: {'Authorization': 'Bearer' + ' ' + cookie.load('token')}
+    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad('token')}
   });
 }
