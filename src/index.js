1b86b71658de891e7433235ee775a0ca8a3d62ad
Sergey Pozhilov <pozhilov@gmail.com>
2018-03-14 11:08:39 +0300

    Code moved from App to index, now with redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { render } from 'react-dom';
-import { App } from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './scenes/Home';
+import LoginPage from './scenes/Auth/Login';
+import FeedsPage from './scenes/Feeds';
+// import SignupPage from './scenes/Auth/Signup';
+// import UserPage from './scenes/User';
+// import SettingsPage from './scenes/Settings';
+// import FeedInfo from './scenes/FeedInfo';
+import store from 'store';
+
 import 'assets/styles/main.scss';
 
-render(
-  <App />,
+render (
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/feeds" component={FeedsPage}/>
+        <Route exact path="/login" component={LoginPage}/>
+      </div>
+    </Router>
+  </Provider>,
   document.getElementById('app')
 );
