7170d2a0c051774babf605d3eba06fa752b220d3
Sergey Pozhilov <pozhilov@gmail.com>
2018-03-15 09:20:22 +0300

    new helper function for string pluralizign; code styling in utils/converter

--- src/utils/converter.js
+++ src/utils/converter.js
@@ -1,23 +1,26 @@
-
 export function serialize(data) {
-    if (typeof(data) != 'object') {
-        return '?' + data;
+  if (typeof(data) != 'object') {
+    return '?' + data;
+  }
+  var str = [];
+  for (var p in data) {
+    if (data[p] && data.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + '=' + encodeURIComponent(data[p]));
     }
-    var str = [];
-    for (var p in data) {
-        if (data[p] && data.hasOwnProperty(p)) {
-            str.push(encodeURIComponent(p) + '=' + encodeURIComponent(data[p]));
-        }
-    }
-    return '?' + str.join('&');
+  }
+  return '?' + str.join('&');
 }
 
 export function getPathParam() {
-    var args = arguments;
-    var params = [];
+  var args = arguments;
+  var params = [];
 
-    for (var a in args) {
-        params.push(args[a]);
-    }
-    return params.join('/');
+  for (var a in args) {
+    params.push(args[a]);
+  }
+  return params.join('/');
+}
+
+export function pluralize(str) {
+  return str.slice(-1) === 'y' ? str.slice(0, -1) + 'ies' : str + 's';
 }
