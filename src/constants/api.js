24a8d39da76a60322302bd56f751a95913276d5e
Sergey Pozhilov <pozhilov@gmail.com>
2018-03-17 23:16:23 +0300

    Constants refactored

---
+++ src/constants/api.js
@@ -0,0 +1,6 @@
+const API_BASE = 'http://localhost:8000/api/';
+
+export const API_URL = API_BASE;
+export const API_LOGIN = API_BASE + 'auth/login';
+export const API_SIGNUP = API_BASE + 'user';
+
---
+++ src/constants/common.js
@@ -0,0 +1,3 @@
+export const ORDER_CATEGORIES = 'categories';
+export const ORDER_POPULAR = 'popular';
+export const ORDER_RECENT = 'recent';
