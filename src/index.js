ef68760402b9edbdded74fea546677fdaef999b5
Sergey Pozhilov <pozhilov@gmail.com>
2018-03-19 08:36:13 +0300

    Error fixed in routes (user scene was always on the screen)

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import history from './history';
 
 import HomePage from './scenes/Home';
 import LoginPage from './scenes/Auth/Login';
 import FeedsPage from './scenes/Feeds';
+import UserPage from './scenes/User';
+
 // import SignupPage from './scenes/Auth/Signup';
-// import UserPage from './scenes/User';
 // import SettingsPage from './scenes/Settings';
 
 import 'assets/styles/main.scss';
@@ -18,14 +19,13 @@ import store from 'store';
 render (
   <Provider store={store}>
     <Router history={history}>
-      <div>
+      <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route path="/feeds" component={FeedsPage}/>
         <Route path="/feed" component={FeedsPage}/>
         <Route exact path="/login" component={LoginPage}/>
-        {/*<Route exact path="/settings" component={SettingsPage}/>*/}
-        {/*<Route path="/subscriptions" component={FeedsPage}/>*/}
-      </div>
+        <Route component={UserPage}/>
+      </Switch>
     </Router>
   </Provider>,
   document.getElementById('app')
