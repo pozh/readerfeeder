1356c212b9411d2ca5d3afd65d4a00adbf7c50de
Sergey Pozhilov <pozhilov@gmail.com>
2018-03-23 09:55:44 +0300

    constants added

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,3 +1,6 @@
 export const ORDER_CATEGORIES = 'categories';
 export const ORDER_POPULAR = 'popular';
 export const ORDER_RECENT = 'recent';
+
+export const USER_HOME = '/subscriptions';
+export const HOME = '/';
--- src/constants/message.js
+++ src/constants/message.js
@@ -1,2 +1,3 @@
 export const DELETE_CONFIRM_MESSAGE = 'Are you sure you want to delete this feed?';
 export const NOT_AUTHORISED = 'You are not authorized to do this. Please login and try again.';
+export const INVALID_LOGIN_DATA = 'Invalid username and password.';
