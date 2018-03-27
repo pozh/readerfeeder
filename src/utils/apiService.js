ccd6dd984e0158cff2797fc9f3b6534418eb5d10
Sergey Pozhilov <pozhilov@gmail.com>
2018-03-27 11:01:04 +0300

    Refactoring: Get rid of apiUtil, move code to apiService
    The code was too generic to keep it as another set of functions.

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -1,21 +1,35 @@
-// Import utils
-import * as ApiUtil from './apiUtil';
+import axios from 'axios';
+import Cookies from 'universal-cookie';
+import { TOKEN } from 'constants/api';
 import * as Converter from './converter';
-
 import * as api from '../constants/api';
 
+
+const cookieLoad = cookieName => {
+  const cookies = new Cookies();
+  return cookies.get(cookieName);
+};
+
 export function fetch(pathParam, data) {
-  return ApiUtil.fetch(api.API_ROOT, pathParam.toLowerCase());
+  return axios.get(api.API_ROOT + pathParam.toLowerCase(), {
+    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad(TOKEN)}
+  });
 }
 
 export function store(resourceName, data) {
-  return ApiUtil.store(api.API_ROOT, resourceName.toLowerCase(), data);
+  return axios.post(api.API_ROOT + pathParam.toLowerCase(), data, {
+    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad(TOKEN)}
+  });
 }
 
 export function update(resourceName, data, dataId) {
-  return ApiUtil.update(api.API_ROOT, Converter.getPathParam(resourceName.toLowerCase(), dataId), data);
+  return axios.put(api.API_ROOT + Converter.getPathParam(resourceName.toLowerCase(), dataId), data, {
+    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad(TOKEN)}
+  });
 }
 
 export function destroy(resourceName, dataId) {
-  return ApiUtil.destroy(api.API_ROOT, Converter.getPathParam(resourceName.toLowerCase(), dataId));
+  return axios.delete(api.API_ROOT + Converter.getPathParam(resourceName.toLowerCase(), dataId), {
+    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad(TOKEN)}
+  });
 }
