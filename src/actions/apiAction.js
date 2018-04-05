bb01ecdccd6ee059f47bda279292f979768d1114
Sergey Pozhilov <pozhilov@gmail.com>
2018-04-05 15:45:28 +0300

    Add "details" option in the apiAction

--- src/actions/apiAction.js
+++ src/actions/apiAction.js
@@ -5,22 +5,25 @@ import * as ActionType from '../constants/actionType';
  */
 
 // Everytime an API request is made, this action gets called
-export function apiRequest() {
-    return {
-        type: ActionType.API_REQUEST
-    }
+export function apiRequest(details) {
+  return {
+    type: ActionType.API_REQUEST,
+    payload: details
+  }
 }
 
 // Everytime a response is received, this action gets called
-export function apiResponse() {
-    return {
-        type: ActionType.API_RESPONSE
-    }
+export function apiResponse(details) {
+  return {
+    type: ActionType.API_RESPONSE,
+    payload: details
+  }
 }
 
 // Everytime a component unmounts, this action gets called
-export function apiClearState() {
-    return {
-        type: ActionType.API_CLEAR_STATE
-    }
+export function apiClearState(details) {
+  return {
+    type: ActionType.API_CLEAR_STATE,
+    payload: details
+  }
 }
