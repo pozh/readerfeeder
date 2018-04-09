6bfd544f86905824904dded3b6da253c0f4d7c2e
Sergey Pozhilov <pozhilov@gmail.com>
2018-04-09 10:14:27 +0300

    Display my subscriptions - Initial implementation

--- src/scenes/User/components/Subscriptions.js
+++ src/scenes/User/components/Subscriptions.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import PageCaption from './../../components/PageCaption';
-import { Container } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import {Container} from 'reactstrap';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import _ from 'lodash';
+import FeedCard from 'scenes/Feeds/components/FeedCard';
 
 import * as apiAction from 'actions/apiAction';
 import * as crudAction from 'actions/crudAction';
@@ -17,27 +18,39 @@ class Subscriptions extends Component {
   }
 
   componentWillMount() {
-    if (!this.props.subscriptions.length > 0) {
-      this.props.actions.fetchAll('subscription');
-    }
+    if (!this.props.feeds.length > 0) this.props.actions.fetchAll('feed');
+    if (!this.props.subscriptions.length > 0) this.props.actions.fetchAll('subscription');
   }
 
   render() {
+    const subs = this.props.subscriptions;
+    const allFeeds = this.props.feeds;
+    const myFeeds = allFeeds.filter(feed => (_.findIndex(subs, sub => (sub.feed_id === feed.id)) >= 0));
+
     return (
       <main>
         <PageCaption>Subscriptions</PageCaption>
         <Container>
-          {!this.props.subscriptions.length > 0 && (
+          {!subs.length > 0 && (
             <div className="text-center">
-              <h3>No subscriptions yet</h3>
-              <p>
-                <br/>
-                <Link className="btn btn-lg btn-round btn-primary" to="/feeds">Browse Feeds</Link>
-              </p>
+              {this.props.apiState.isRequesting && <h3>Loading...</h3>}
+              {!this.props.apiState.isRequesting && (
+                <div>
+                  <h3>No subscriptions yet</h3>
+                  <p>
+                    <br/>
+                    <Link className="btn btn-lg btn-round btn-primary" to="/feeds">Browse Feeds</Link>
+                  </p>
+                </div>
+              )}
             </div>
           )}
-          {this.props.subscriptions.length > 0 && (
-            <p>xxx</p>
+          {subs.length > 0 && (
+            <div className="Feeds-feeds">
+              <ul>
+                {myFeeds.map((feed, idx) => <FeedCard feed={feed} key={idx}/>)}
+              </ul>
+            </div>
           )}
         </Container>
       </main>
@@ -51,7 +64,7 @@ class Subscriptions extends Component {
  */
 function mapStateToProps(state) {
   return {
-
+    feeds: state.crud.items.feeds,
     subscriptions: state.crud.items.subscriptions,
     apiState: state.api
   }
