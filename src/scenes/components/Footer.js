7626c7d77e7d87f75fde88654c8c05c588bca281
Sergey Pozhilov <pozhilov@gmail.com>
2018-05-18 11:11:40 +0300

    Links and auto-year in the footer

--- src/scenes/components/Footer.js
+++ src/scenes/components/Footer.js
@@ -10,12 +10,10 @@ export const Footer = () => (
       <Container>
         <Link className="navbar-brand" to="/"><img src={require("../../assets/images/logo-footer.png")} alt="" /></Link>
         <Nav navbar className="in">
-          <NavItem><NavLink to="/about">About Us</NavLink></NavItem>
-          <NavItem><NavLink to="/pricing">Pricing</NavLink></NavItem>
           <NavItem><NavLink to="/terms">Terms of Use</NavLink></NavItem>
-          <NavItem><NavLink to="/pricing">Privacy Policy</NavLink></NavItem>
+          <NavItem><NavLink to="/privacy">Privacy Policy</NavLink></NavItem>
         </Nav>
-        <span className="ml-auto text-right">&copy; Copyright 2018 ReaderFeeder</span>
+        <span className="ml-auto text-right">ReaderFeeder &copy; {(new Date().getFullYear())}</span>
       </Container>
     </Navbar>
   </footer>
