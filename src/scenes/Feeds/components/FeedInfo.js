aea69afa1e38a511f45ee1644c92777cd8bdd0e8
Sergey Pozhilov <pozhilov@gmail.com>
2018-07-21 11:47:41 +0300

    Eslinted

--- src/scenes/Feeds/components/FeedInfo.js
+++ src/scenes/Feeds/components/FeedInfo.js
@@ -1,95 +1,85 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import DocumentTitle from 'react-document-title';
 
-import {bindActionCreators} from 'redux';
-import {Container, Row, Col} from 'reactstrap';
-import PageCaption from './../../components/PageCaption';
-import FeedToc from './FeedToc';
-import {isEmpty} from "../../../utils/commonUtil";
-
+import { bindActionCreators } from 'redux';
+import { Container, Row, Col } from 'reactstrap';
 import * as apiAction from 'actions/apiAction';
 import * as crudAction from 'actions/crudAction';
-import {TITLE_SUFFIX} from "../../../constants/common";
+
+import PageCaption from './../../components/PageCaption';
+import { isEmpty } from '../../../utils/commonUtil';
+
+import { TITLE_SUFFIX } from '../../../constants/common';
 
 
 class FeedInfo extends Component {
-
   componentWillMount() {
-    let feed = this.props.feed;
-    let slug = this.props.match.params.slug;
+    const feed = this.props.feed;
+    const slug = this.props.match.params.slug;
 
     if (isEmpty(feed) || feed.slug !== slug) {
       this.props.actions.fetchBySlug('feed', slug);
     }
-  };
+  }
 
   render() {
-
     const items = [];
     const feed = this.props.feed;
-    const pageTitle = feed.title + ' - Kindle subscription' + TITLE_SUFFIX;
+    const pageTitle = `${feed.title} - Kindle subscription${TITLE_SUFFIX}`;
     // const items = this.state.items;
 
-    if (!feed || this.props.match.params.slug !== feed.slug)
+    if (!feed || this.props.match.params.slug !== feed.slug) {
       return (
         <main>
           <PageCaption>...</PageCaption>
-          <Container className="mt-5"></Container>
+          <Container className="mt-5" />
         </main>
       );
-    else return (
+    }
+    return (
       <DocumentTitle title={pageTitle}>
-      <main>
-        <PageCaption>{feed.title}</PageCaption>
-        <Container>
-          <Row className="mt-5">
-            <Col sm="9">
-              {feed.description && (
-                <div className="mb-5">{feed.description}</div>
-              )}
-              <div className="mb-5">
-                <h3>Sources:</h3>
-                <ul>
-                  {feed.sources && feed.sources.map((source, idx) => <li key={idx}>{source.url}</li>)}
-                </ul>
-              </div>
-              {/*<FeedToc toc={items} />*/}
-            </Col>
-            <Col sm="3">
-              <p><Link to="#" className="btn btn-lg btn-block btn-primary">Subscribe</Link></p>
-            </Col>
-          </Row>
-        </Container>
-      </main>
+        <main>
+          <PageCaption>{feed.title}</PageCaption>
+          <Container>
+            <Row className="mt-5">
+              <Col sm="9">
+                {feed.description && (
+                  <div className="mb-5">{feed.description}</div>
+                )}
+                <div className="mb-5">
+                  <h3>Sources:</h3>
+                  <ul>
+                    {feed.sources && feed.sources.map(source =>
+                      <li key={source.url}>{source.url}</li>)}
+                  </ul>
+                </div>
+                {/* <FeedToc toc={items} /> */}
+              </Col>
+              <Col sm="3">
+                <p><Link to="#" className="btn btn-lg btn-block btn-primary">Subscribe</Link></p>
+              </Col>
+            </Row>
+          </Container>
+        </main>
       </DocumentTitle>
     );
+  }
+}
+
+
+function mapStateToProps(state) {
+  return {
+    feed: state.crud.selectedItem.feed,
+    apiState: state.api
   };
 }
 
-
-/**
- * Map the state to props.
- */
-function mapStateToProps(state) {
-  return {
-    feed: state.crud.selectedItem['feed'],
-    apiState: state.api
-  }
-}
-
-
-/**
- * Map the actions to props.
- */
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(_.assign({}, crudAction, apiAction), dispatch)
-  }
+  };
 }
 
-/**
- * Connect the component to the Redux store.
- */
 export default connect(mapStateToProps, mapDispatchToProps)(FeedInfo);
--- src/scenes/User/index.js
+++ src/scenes/User/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Route, Switch, Link} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import DocumentTitle from 'react-document-title';
-import {TITLE_USER_SETTINGS} from "../../constants/common";
+import { TITLE_USER_SETTINGS } from '../../constants/common';
 import Footer from './../components/Footer';
 import Header from './../components/Header';
 import RequireAuth from '../Auth/RequireAuth';
@@ -11,20 +11,18 @@ import Pro from './components/Pro';
 
 import './styles.scss';
 
-export const UserPage = props => {
-  return (
-    <DocumentTitle title={TITLE_USER_SETTINGS}>
+export const UserPage = props => (
+  <DocumentTitle title={TITLE_USER_SETTINGS}>
     <div>
-      <Header className="white"/>
+      <Header className="white" />
       <Switch>
-        <Route path="/subscriptions" component={RequireAuth(Subscriptions)}/>
-        <Route path="/settings" component={RequireAuth(Settings)}/>
-        <Route path="/pro" component={RequireAuth(Pro)}/>
+        <Route path="/subscriptions" component={RequireAuth(Subscriptions)} />
+        <Route path="/settings" component={RequireAuth(Settings)} />
+        <Route path="/pro" component={RequireAuth(Pro)} />
       </Switch>
-      <Footer/>
+      <Footer />
     </div>
-    </DocumentTitle>
-  );
-};
+  </DocumentTitle>
+);
 
 export default UserPage;
