2b0dfaf2c1112493d5bf9e59c90717fa1e16722d
Sergey Pozhilov <pozhilov@gmail.com>
2018-07-21 11:00:32 +0300

    new api root URL

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,4 @@
-export const API_ROOT = 'http://localhost:8000/api/';
+export const API_ROOT = 'http://rf.local/api/';
 
 export const API_LOGIN = API_ROOT + 'auth/login';
 export const API_SIGNUP = API_ROOT + 'user';
