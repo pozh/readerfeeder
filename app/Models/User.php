e59cd2a410cff7149a4056513a44217c6e2ad9e7
Sergey Pozhilov <pozhilov@gmail.com>
2018-07-27 16:13:35 +0300

    User model - got rid of errors after switching to Laravel. WIP

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,15 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Auth\Authenticatable;
-use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Tymon\JWTAuth\Contracts\JWTSubject as JWTSubjectContract;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubjectContract
+//use Illuminate\Foundation\Auth\User as Authenticatable;
+
+
+class User extends Model
 {
-    use Authenticatable, Authorizable;
+    use Authenticatable;
 
     /**
      * The attributes that are mass assignable.
