3963dc8f5b25d21da6f7914e06319f3289993b65
Sergey Pozhilov <pozhilov@gmail.com>
2018-08-06 08:42:31 +0300

    Category Resource updated: returns correct data array

--- app/Http/Resources/Category.php
+++ app/Http/Resources/Category.php
@@ -14,6 +14,13 @@ class Category extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'slug' => $this->slug,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
