36a5e76b8ed07663871b919c12ccd1d569593155
Sergey Pozhilov <pozhilov@gmail.com>
2018-08-07 17:22:11 +0300

    Signup API endpoint changed

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,6 +1,6 @@
 export const API_ROOT = 'http://rf.local/api/';
 
 export const API_LOGIN = API_ROOT + 'auth/login';
-export const API_SIGNUP = API_ROOT + 'user';
+export const API_SIGNUP = API_ROOT + 'auth/register';
 export const API_ME = API_ROOT + 'user/me';
 export const TOKEN = 'token';
