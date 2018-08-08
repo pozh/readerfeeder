27d35eda93a62078f5ee2c48bc9c1afd5b83e976
Sergey Pozhilov <pozhilov@gmail.com>
2018-08-08 11:47:51 +0300

    User: resource class finished

--- app/Http/Resources/User.php
+++ app/Http/Resources/User.php
@@ -14,6 +14,10 @@ class User extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'first_name' => $this->first_name,
+            'email' => $this->email
+        ];
     }
 }
