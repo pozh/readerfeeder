1e3c27ade6a738cf43c284c4f9d5b71a4f8ff345
Sergey Pozhilov <pozhilov@gmail.com>
2018-08-08 11:47:34 +0300

    Subscription: resource class finished

--- app/Http/Resources/Subscription.php
+++ app/Http/Resources/Subscription.php
@@ -14,6 +14,11 @@ class Subscription extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'feed_id' => $this->feed_id,
+            'status' => $this->status
+        ];
     }
 }
