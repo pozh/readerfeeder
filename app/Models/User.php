d5dfb13360f2a7ab21e78214421a7402cbe71a57
Sergey Pozhilov <pozhilov@gmail.com>
2018-09-03 15:21:24 +0300

    hide few fields in User model from API

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'email_token', 'deleted_at', 'email_validation_date'
     ];
 
     /**
