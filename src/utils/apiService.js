0698730dd4e112e3579d7742847db201df1de0d9
Sergey Pozhilov <pozhilov@gmail.com>
2018-09-03 13:39:12 +0300

    Eslint, code optimization

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -12,30 +12,32 @@ const cookieLoad = cookieName => {
 
 export function getAuthorized(path) {
   return axios.get(path, {
-    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad(TOKEN)}
+    headers: { Authorization: `Bearer ${cookieLoad(TOKEN)}` }
   });
 }
 
-export function fetch(pathParam, data) {
+export function fetch(pathParam) {
   return axios.get(api.API_ROOT + pathParam.toLowerCase(), {
-    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad(TOKEN)}
+    headers: { Authorization: `Bearer ${cookieLoad(TOKEN)}` }
   });
 }
 
 export function store(resourceName, data) {
   return axios.post(api.API_ROOT + resourceName.toLowerCase(), data, {
-    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad(TOKEN)}
+    headers: { Authorization: `Bearer ${cookieLoad(TOKEN)}` }
   });
 }
 
 export function update(resourceName, data, dataId) {
-  return axios.put(api.API_ROOT + Converter.getPathParam(resourceName.toLowerCase(), dataId), data, {
-    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad(TOKEN)}
+  const pathParam = Converter.getPathParam(resourceName.toLowerCase(), dataId);
+  return axios.put(api.API_ROOT + pathParam, data, {
+    headers: { Authorization: `Bearer ${cookieLoad(TOKEN)}` }
   });
 }
 
 export function destroy(resourceName, dataId) {
-  return axios.delete(api.API_ROOT + Converter.getPathParam(resourceName.toLowerCase(), dataId), {
-    headers: {'Authorization': 'Bearer' + ' ' + cookieLoad(TOKEN)}
+  const pathParam = Converter.getPathParam(resourceName.toLowerCase(), dataId);
+  return axios.delete(api.API_ROOT + pathParam, {
+    headers: { Authorization: `Bearer ${cookieLoad(TOKEN)}` }
   });
 }
