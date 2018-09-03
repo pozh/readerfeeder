63cd7a6431b42af9991b82fa0682a0a5f77c260a
Sergey Pozhilov <pozhilov@gmail.com>
2018-09-03 15:17:40 +0300

    Refactor: Got rid of useless constants

--- src/constants/actionType.js
+++ src/constants/actionType.js
@@ -1,6 +1,5 @@
 export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
 export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
-export const RECEIVE_REFRESH_TOKEN = 'RECEIVE_REFRESH_TOKEN';
 export const LOG_OUT = 'LOG_OUT';
 
 export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
--- src/constants/api.js
+++ src/constants/api.js
@@ -2,5 +2,4 @@ export const API_ROOT = 'http://rf.local/api/';
 
 export const API_LOGIN = API_ROOT + 'auth/login';
 export const API_SIGNUP = API_ROOT + 'auth/register';
-export const API_ME = API_ROOT + 'user/me';
 export const TOKEN = 'token';
