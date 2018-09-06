1ef28d5c9a6173975ddda35d5d3eb1583eb0c63f
Sergey Pozhilov <pozhilov@gmail.com>
2018-09-06 15:11:04 +0300

    Paddle integration: initial commit, WIP

--- src/scenes/User/components/Pricing.js
+++ src/scenes/User/components/Pricing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
 import DocumentTitle from 'react-document-title';
 import * as apiAction from 'actions/apiAction';
 import * as authAction from 'actions/authAction';
@@ -13,20 +14,31 @@ class Pricing extends Component {
   }
 
   render() {
-    const user = this.props.user;
+    const isAuthenticated = this.props.isAuthenticated;
+    const usermeta = this.props.usermeta;
+    const isPro = isAuthenticated && (usermeta.plan === 'pro');
+
+    let freeBtn = <Link to="/signup" className="paddle_button">Choose Plan</Link>;
+    let proBtn = freeBtn;
+    if (isAuthenticated) {
+      freeBtn = isPro ? <a href="#!" data-product="530517" className="paddle_button">Choose Plan</a> : <span>Current plan</span>;
+      proBtn = isPro ? <span>Current plan</span> : <a href="#!" data-product="530517" className="btn btn-secondary">Choose Plan</a>;
+    }
+
     return (
       <DocumentTitle title={TITLE_PRICING}>
         <main>
           <PageCaption>
             Choose your plan
-            <small>Find the plan that suits your needs</small>
+            <small>Use ReaderFeeder for free, or go Pro to unlock all its features</small>
           </PageCaption>
 
           <div className="section pricing">
             <div className="container">
               <div className="row justify-content-center">
+
                 <div className="col-lg-4">
-                  <div className="pricing-card">
+                  <div className={`pricing-card ${isPro || !isAuthenticated ? 'pricing-card_spec' : ''}`}>
                     <h3 className="pricing-card__header">Free</h3>
                     <h4 className="pricing-card__title">Basic Reader</h4>
                     <ul className="pricing-card__features">
@@ -35,24 +47,26 @@ class Pricing extends Component {
                       <li>Visual Composer Included</li>
                       <li>24/7 Support System</li>
                     </ul>
-                    <a href="#" className="btn btn-secondary">Choose Plan</a>
+                    {freeBtn}
                   </div>
                 </div>
+
                 <div className="col-xs-12 col-lg-4">
-                  <div className="pricing-card">
+                  <div className={`pricing-card ${!isPro || !isAuthenticated ? 'pricing-card_spec' : ''}`}>
                     <h3 className="pricing-card__header">
-                      <span className="currency">$</span>9<span className="period">/month</span>
+                      <span className="currency">$</span>7<span className="period">/month</span>
                     </h3>
-                    <h4 className="pricing-card__title">Power Reader</h4>
+                    <h4 className="pricing-card__title">Pro Reader</h4>
                     <ul className="pricing-card__features">
                       <li>Ultimate Features</li>
                       <li>Responsive Ready</li>
                       <li>Visual Composer Included</li>
                       <li>24/7 Support System</li>
                     </ul>
-                    <a href="#" className="btn btn-secondary">Choose Plan</a>
+                    {proBtn}
                   </div>
                 </div>
+
               </div>
             </div>
           </div>
@@ -68,7 +82,9 @@ class Pricing extends Component {
  */
 function mapStateToProps(state) {
   return {
+    isAuthenticated: state.auth.isAuthenticated,
     user: state.auth.user,
+    usermeta: state.auth.usermeta,
     apiState: state.api
   };
 }
@@ -86,6 +102,5 @@ function mapDispatchToProps(dispatch) {
 /**
  * Connect the component to the Redux store.
  */
-
 export default connect(mapStateToProps, mapDispatchToProps)(Pricing);
 
