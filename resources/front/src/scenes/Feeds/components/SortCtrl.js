55109df679f91e8ff61b3f0fa5dfc58386374e84
Sergey Pozhilov <pozhilov@gmail.com>
2018-09-17 16:40:48 +0300

    Feeds sorting control fixed: did not display which option is active

--- resources/front/src/scenes/Feeds/components/SortCtrl.js
+++ resources/front/src/scenes/Feeds/components/SortCtrl.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import h from '../../../history';
-import * as c from 'constants/common';
 
-const SortCtrl = props => {
+const SortCtrl = () => {
 
-  let order = c.ORDER_CATEGORIES;
-  if (h.location.pathname == '/feeds/popular') order = c.ORDER_POPULAR;
-  else if (h.location.pathname == '/feeds/recent') order = c.ORDER_RECENT;
+  let order = 'categories';
+  if (h.location.pathname === '/feeds/popular') order = 'popular';
+  else if (h.location.pathname === '/feeds/recent') order = 'recent';
 
   return (
     <ul className="Feeds-order">
-      <li><Link className={order===c.ORDER_CATEGORIES ? 'active' : ''} to="/feeds">Categories</Link></li>
-      <li><Link className={order===c.ORDER_POPULAR ? 'active' : ''} to="/feeds/popular">Most popular</Link></li>
-      <li><Link className={order===c.ORDER_RECENT ? 'active' : ''} to="/feeds/recent">Recent</Link></li>
+      <li><Link className={order==='categories' ? 'active' : ''} to="/feeds">Categories</Link></li>
+      <li><Link className={order==='popular' ? 'active' : ''} to="/feeds/popular">Most popular</Link></li>
+      <li><Link className={order==='recent' ? 'active' : ''} to="/feeds/recent">Recent</Link></li>
     </ul>
   );
-}
+};
 
 export default SortCtrl;
