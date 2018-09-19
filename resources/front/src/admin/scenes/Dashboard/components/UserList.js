f1cb52c2def1d3a0ec2ab18898aab9230cfd2dfb
Sergey Pozhilov <pozhilov@gmail.com>
2018-09-19 19:01:16 +0300

    functional UserList

--- resources/front/src/admin/scenes/Dashboard/components/UserList.js
+++ resources/front/src/admin/scenes/Dashboard/components/UserList.js
@@ -1,23 +1,27 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import _ from 'lodash';
+
+import * as apiAction from 'actions/apiAction';
+import * as crudAction from 'actions/crudAction';
 
 class UserList extends Component {
   static defaultProps = {};
   static propTypes = {};
 
   componentDidMount() {
-    this.state.users = [];
+    this.props.actions.fetchAll('user');
   }
 
-  state = {
-    users: []
-  };
-
   render() {
-    if (!this.state.users.count > 0) return (<p>No users</p>);
+    const users = this.props.users || [];
+    if (!users.length > 0) return (<p>No users</p>);
     else return (
       <div>
-        <h2>Users : {this.state.users.count}</h2>
+        <h2>Users ({users.length})</h2>
         <table className="table">
           <thead>
           <tr>
@@ -25,12 +29,19 @@ class UserList extends Component {
             <th scope="col">Name</th>
             <th scope="col">Plan</th>
             <th scope="col">email</th>
-            <th scope="col">kindle email</th>
             <th scope="col">Settings</th>
           </tr>
           </thead>
           <tbody>
-
+            {users.map(user => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.first_name}</td>
+                <td>&nbsp;</td>
+                <td>{user.email}</td>
+                <td>{user.settings}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
@@ -38,4 +49,16 @@ class UserList extends Component {
   }
 }
 
-export default UserList;
+function mapStateToProps(state) {
+  return {
+    users: state.crud.items.users
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(_.assign({}, crudAction, apiAction), dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserList);
