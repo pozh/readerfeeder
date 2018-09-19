e1da8ac2d7485597e252eb30123ed303bae9c5cc
Sergey Pozhilov <pozhilov@gmail.com>
2018-09-19 17:27:40 +0300

    Dashboard component - initial commit

---
+++ resources/front/src/admin/scenes/Dashboard/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import DocumentTitle from 'react-document-title';
+import { Link } from 'react-router-dom';
+import UserList from './components/UserList';
+
+
+const Dashboard = () => (
+  <DocumentTitle title="RSS delivery service for Kindle Paperwhite, Voyage and Oasis - ReaderFeeder">
+    <div>
+      <div className="container pt-5">
+        <h1>ADMIN</h1>
+        <hr/>
+        <h4 className="text-right">
+          <Link to="users">Users</Link> | <Link to="feeds">Feeds</Link>
+        </h4>
+        <UserList/>
+      </div>
+    </div>
+  </DocumentTitle>
+);
+
+export default Dashboard;
