90d2358470a01bfe1e738aebe2ac3fbb6b7c4126
Sergey Pozhilov <pozhilov@gmail.com>
2018-09-25 14:59:23 +0300

    DB seeder optimized

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,5 @@ class DatabaseSeeder extends Seeder
         $this->call(RoleTableSeeder::class);
         $this->call(UserRoleSeeder::class);
         $this->call(FeedSeeder::class);
-        $this->call(CategorySeeder::class);
     }
 }
--- database/seeds/FeedSeeder.php
+++ database/seeds/FeedSeeder.php
@@ -3,6 +3,10 @@
 use Illuminate\Database\Seeder;
 use Carbon\Carbon as Carbon;
 
+use App\Models\Feed;
+use App\Models\Source;
+use App\Models\Category;
+
 class FeedSeeder extends Seeder
 {
     /**
@@ -14,38 +18,58 @@ class FeedSeeder extends Seeder
     {
         $path = storage_path() . "/json/db2.json";
         $json = json_decode(file_get_contents($path), true);
-        $feeds = [];
-        $sources = [];
-        foreach ($json['feeds'] as $feed) {
-            $feeds[] = [
-                'id' => $feed['id'],
-                'title' => $feed['title'],
-                'user_id' => 1,
-                'status' => 'active',
-                'period' => $feed['period'],
-                'schedule_day' => $feed['schedule_day'],
-                'schedule_time' => $feed['schedule_time'],
-                'type' => $feed['type'],
-                'category_id' => !empty($feed['category']) ? $feed['category'] + 12 : 0,
-                'description' => !empty($feed['description']) ? $feed['description'] : '',
-                'slug' => $feed['slug'],
-                'subscribers' => $feed['subscribers'],
-                'created_at' => Carbon::now()
-            ];
-            if (!empty($feed['sources'])) {
-                foreach ($feed['sources'] as $source){
-                    $sources[] = [
-                        'feed_id' => $feed['id'],
-                        'title' => '',
-                        'url' => $source['url'],
-                        'count' => $source['count'],
-                        'created_at' => Carbon::now(),
-                        'order' => 0
-                    ];
+
+        $categories = [
+            ['title' => 'News - World', 'slug' => 'news-world', 'description' => '', 'created_at' => Carbon::now()],
+            ['title' => 'Religion', 'slug' => 'religion', 'description' => '', 'created_at' => Carbon::now()],
+            ['title' => 'Science', 'slug' => 'science', 'description' => '', 'created_at' => Carbon::now()],
+            ['title' => 'Deutschland News', 'slug' => 'deutschland', 'description' => '', 'created_at' => Carbon::now()],
+            ['title' => 'News and Politics', 'slug' => 'news-and-politics', 'description' => '', 'created_at' => Carbon::now()],
+            ['title' => 'General Reading', 'slug' => 'reading', 'description' => '', 'created_at' => Carbon::now()],
+            ['title' => 'Tech', 'slug' => 'tech', 'description' => '', 'created_at' => Carbon::now()],
+            ['title' => 'Lifestyle & Entertainment', 'slug' => 'lifestyle', 'description' => '', 'created_at' => Carbon::now()],
+            ['title' => 'Noticias de España', 'slug' => 'espana', 'description' => '', 'created_at' => Carbon::now()],
+            ['title' => 'News - US', 'slug' => 'us', 'description' => '', 'created_at' => Carbon::now()],
+            ['title' => 'Nouvelles en France', 'slug' => 'france', 'description' => '', 'created_at' => Carbon::now()],
+            ['title' => 'Business & Economy', 'slug' => 'business', 'description' => '', 'created_at' => Carbon::now()]
+        ];
+
+        foreach ($categories as $catIdx => $cat) {
+            $catID = Category::create($cat)->id;
+            foreach ($json['feeds'] as $feed) {
+                $feedCategoryID = empty($feed['category']) ? 1 : $feed['category'];
+                if ($feedCategoryID != $catIdx + 1) continue;
+                $data = [
+                    'title' => $feed['title'],
+                    'user_id' => 1,
+                    'status' => 'active',
+                    'period' => $feed['period'],
+                    'schedule_day' => $feed['schedule_day'],
+                    'schedule_time' => $feed['schedule_time'],
+                    'type' => $feed['type'],
+                    'category_id' => $feedCategoryID,
+                    'description' => empty($feed['description']) ? '' : $feed['description'],
+                    'slug' => $feed['slug'],
+                    'subscribers' => 0,
+                    'created_at' => Carbon::now()
+                ];
+                $feedID = Feed::create($data)->id;
+
+                if (!empty($feed['sources'])) {
+                    foreach ($feed['sources'] as $source) {
+                        $data = [
+                            'feed_id' => $feedID,
+                            'title' => '',
+                            'url' => $source['url'],
+                            'count' => $source['count'],
+                            'created_at' => Carbon::now(),
+                            'order' => 0
+                        ];
+                        Source::create($data);
+                    }
                 }
             }
         }
-        DB::table('feeds')->insert($feeds);
-        DB::table('sources')->insert($sources);
+
     }
 }
