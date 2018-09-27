6087ab8496ca4055768d18fd0dbbb63e6f0edfa0
Sergey Pozhilov <pozhilov@gmail.com>
2018-09-27 20:42:28 +0300

    Revert "Got rid of ProcessFeeds Scheduled task"
    This reverts commit 52d05e50e6163ed6084661a83d2da0027328661b.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
+        Commands\processFeeds::class,
     ];
 
     /**
@@ -23,6 +24,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $log_filename = storage_path('logs/feeds.process.log');
+        $schedule->command('feeds:process')
+            ->hourly()
+            ->appendOutputTo($log_filename);
     }
 
     /**
