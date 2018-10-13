2f9fc290b33180a3fef8ad1227d60c5fb8cc58b3
Sergey Pozhilov <pozhilov@gmail.com>
2018-10-13 20:59:09 +0300

    ScrollToTop utility component

---
+++ resources/front/src/shared/utils/ScrollToTop.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class ScrollToTop extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default ScrollToTop;
