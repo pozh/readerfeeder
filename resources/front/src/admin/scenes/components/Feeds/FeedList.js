7a80291dc027947cb13975df1eca8e3f8ba37471
Sergey Pozhilov <pozhilov@gmail.com>
2018-10-13 20:55:08 +0300

    FeedList (feeds table styling). WIP.

--- resources/front/src/admin/scenes/components/Feeds/FeedList.js
+++ resources/front/src/admin/scenes/components/Feeds/FeedList.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
 import _ from 'lodash';
 import ReactTable from "react-table";
 
-import Popup from '../Popup/Popup';
-import FeedEditor from './FeedEditor';
 import * as apiAction from 'actions/apiAction';
 import * as crudAction from 'actions/crudAction';
+import Popup from '../Popup/Popup';
+import FeedEditor from './FeedEditor';
 
 
 class FeedList extends Component {
@@ -16,34 +17,57 @@ class FeedList extends Component {
     this.state = {
       feedPopup: false
     };
-    this.feedPopup;
-    this.columns = [{
-      Header: 'ID',
-      accessor: 'id'
-    }, {
-      Header: 'Title',
-      accessor: 'title',
-    }, {
-      Header: 'Status',
-      accessor: 'status'
-    }, {
-      Header: 'Period',
-      accessor: 'period'
-    }, {
-      Header: 'Subscribers',
-      accessor: 'subscribers'
-    }, {
-      Header: 'Delivery time',
-      accessor: 'schedule_time'
-    }, {
-      Header: 'Category',
-      accessor: 'category_id'
-    }];
+
+    this.columns = [
+      {
+        Header: 'ID',
+        accessor: 'id',
+        className: 'text-center',
+        maxWidth: 50,
+      }, {
+        Header: 'Title',
+        accessor: 'title',
+        minWidth: 250,
+        Cell: (row) => (
+          <Link to="#" onClick={this.onEdit}>{ row.value }</Link>
+        )
+      }, {
+        Header: 'Status',
+        className: 'text-center',
+        accessor: 'status'
+      }, {
+        Header: 'Period',
+        className: 'text-center',
+        accessor: 'period'
+      }, {
+        Header: 'Subscribers',
+        className: 'text-center',
+        maxWidth: 50,
+        accessor: 'subscribers'
+      }, {
+        Header: 'Delivery time',
+        className: 'text-center',
+        maxWidth: 50,
+        accessor: 'schedule_time'
+      }, {
+        Header: 'Category',
+        accessor: 'category_id'
+      }
+    ];
+
+    this.onEdit = this.onEdit.bind(this);
   }
 
   componentDidMount() {
-    this.props.actions.fetchAll('feed');
-    this.props.actions.fetchAll('category');
+    if (!this.props.feeds.length > 0) {
+      this.props.actions.fetchAll('feed');
+      this.props.actions.fetchAll('category');
+    }
+  }
+
+  onEdit(event) {
+    event.preventDefault();
+    alert ('EDIT!');
   }
 
   render() {
@@ -53,7 +77,7 @@ class FeedList extends Component {
       <div>
         <div className="card-header">
           <div className="row">
-            <h5 className="col pt-2"><strong>FEEDS</strong> ({feeds.length})</h5>
+            <h5 className="col pt-2">FEEDS ({feeds.length})</h5>
             <div className="col text-right">
               <Popup title="New Feed">
                 <FeedEditor/>
@@ -69,7 +93,8 @@ class FeedList extends Component {
 
 function mapStateToProps(state) {
   return {
-    feeds: state.crud.items.feeds
+    feeds: state.crud.items.feeds,
+    categories: state.crud.items.categories
   };
 }
 
