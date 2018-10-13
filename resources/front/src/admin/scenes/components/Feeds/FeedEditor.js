a33257da4d117ddc2e401c97259ff2ce9789c567
Sergey Pozhilov <pozhilov@gmail.com>
2018-10-13 20:53:59 +0300

    react-final-form in feed editor. WIP

--- resources/front/src/admin/scenes/components/Feeds/FeedEditor.js
+++ resources/front/src/admin/scenes/components/Feeds/FeedEditor.js
@@ -1,53 +1,78 @@
-import React from 'react';
-import FeedInput from './FeedInput';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Field } from 'react-final-form';
+import FeedInput from './FeedInput';
 
-const FeedEditor = () => (
-  <div>
-    <div className="panel-body">
-      <div className="row">
-        <div className="col-md-8">
-          <div className="form-group">
-            <h5><strong>Title</strong></h5>
-            <input className="form-control" id="form_title" type="text"/>
-          </div>
-          <h5 className="mt-3"><strong>Feeds</strong> (URL/Count)</h5>
-          <FeedInput/>
-          <FeedInput/>
-          <FeedInput/>
-          <FeedInput/>
-          <Link className="btn mt-2 btn-secondary" to="">Add Feed</Link>
-        </div>
 
-        <div className="col">
-          <h5><strong>Schedule</strong></h5>
-          <div className="form-group">
-            <select className="form-control" name="schedule_period" id="schedule_period"><option value="daily" selected="selected">Send daily</option><option value="weekly">Send weekly</option><option value="monthly">Send monthly</option></select>
-          </div>
-          <div className="form-group">
-            <select className="form-control" name="schedule_day"><option value="0" selected="selected">Sunday</option><option value="1">Monday</option><option value="2">Tuesday</option><option value="3">Wednesday</option><option value="4">Thursday</option><option value="5">Friday</option><option value="6">Saturday</option></select>
-          </div>
+class FeedEditor extends Component {
 
-          <h5 className="mt-4"><strong>Options</strong></h5>
-          <div className="form-group">
-            <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="keep_images" name="keep_images"/>
-              <label className="form-check-label" for="keep_images">Keep images</label>
-            </div>
-            <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="send_as_book" name="send_as_book"/>
-              <label className="form-check-label" for="send_as_book">Send as book</label>
-            </div>
-          </div>
-        </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      feed: props.feed ? props.feed : {
+        title: '',
+        sources: [],
+
+      }
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit() {
+  }
+
+  render() {
+    return (
+      <div className="panel-body">
+        <Form
+          onSubmit={this.onSubmit}
+          initialValues={{ feedTitle: 'larry' }}
+          render={({ handleSubmit, form, submitting, pristine, values }) => (
+            <form onSubmit={handleSubmit}>
+              <div className="row">
+                <div className="col-md-8">
+                  <div className="form-group">
+                    <h5><strong>Title</strong></h5>
+                    <Field name="feedTitle" component="input" className="form-control" placeholder="Title" />
+                  </div>
+                  <h5 className="mt-3"><strong>Feeds</strong> (URL/Count)</h5>
+                  <FeedInput/>
+                  <Link className="btn mt-2 btn-secondary" to="">Add Feed</Link>
+                </div>
+
+                <div className="col">
+                  <h5><strong>Schedule</strong></h5>
+                  <div className="form-group">
+                    <select className="form-control" name="schedule_period" id="schedule_period"><option value="daily" selected="selected">Send daily</option><option value="weekly">Send weekly</option><option value="monthly">Send monthly</option></select>
+                  </div>
+                  <div className="form-group">
+                    <select className="form-control" name="schedule_day"><option value="0" selected="selected">Sunday</option><option value="1">Monday</option><option value="2">Tuesday</option><option value="3">Wednesday</option><option value="4">Thursday</option><option value="5">Friday</option><option value="6">Saturday</option></select>
+                  </div>
+
+                  <h5 className="mt-4"><strong>Options</strong></h5>
+                  <div className="form-group">
+                    <div className="form-check">
+                      <input className="form-check-input" type="checkbox" value="" id="keep_images" name="keep_images"/>
+                      <label className="form-check-label" for="keep_images">Keep images</label>
+                    </div>
+                    <div className="form-check">
+                      <input className="form-check-input" type="checkbox" value="" id="send_as_book" name="send_as_book"/>
+                      <label className="form-check-label" for="send_as_book">Send as book</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <hr/>
+              <div className="text-right">
+                <button className="btn btn-primary" type="submit" disabled={submitting || pristine}>Submit</button>
+                <button className="btn btn-link" type="button" onClick={form.reset} disabled={submitting || pristine}>Reset</button>
+              </div>
+            </form>
+          )}
+        />
       </div>
-      <hr/>
-      <div className="text-right">
-        <a id="add_cancel" className="btn btn-link" href="javascript:;">Cancel</a>
-        <input type="submit" className="btn btn-primary" name="add_periodical" value="Submit"/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default FeedEditor;
--- resources/front/src/admin/scenes/components/Feeds/FeedInput.js
+++ resources/front/src/admin/scenes/components/Feeds/FeedInput.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Trash as IcoTrash } from 'react-feather';
+import { Field } from 'react-final-form'
 
 const FeedInput = () => (
   <div className="form-row mb-1">
     <div className="col-md-9">
-      <input type="text" className="form-control"/>
+      <Field name="feedUrl" component="input" className="form-control" placeholder="URL" />
     </div>
     <div className="col">
-      <input type="text" className="form-control"/>
+      <Field name="feedCnt" component="input" className="form-control" placeholder="Cnt" />
     </div>
     <div className="col">
       <button className="btn btn-sm btn-link" type="button"><IcoTrash/></button>
