5bfa842a2008c2b6f19f9e898fbf91249a6b7a32
Sergey Pozhilov <pozhilov@gmail.com>
2018-10-16 14:43:07 +0300

    Bug fixed: Token clearance on logout

--- resources/front/src/shared/utils/authUtil.js
+++ resources/front/src/shared/utils/authUtil.js
@@ -3,15 +3,15 @@ import { TOKEN } from 'constants/api';
 
 export const setToken = token => {
   const cookies = new Cookies();
-  cookies.set(TOKEN, token, {path: '/'});
+  cookies.set(TOKEN, token, { path: '/' });
 };
 
 export const clearToken = () => {
   const cookies = new Cookies();
-  cookies.remove(TOKEN);
+  cookies.remove(TOKEN, { path: '/' });
 };
 
 export const getToken = () => {
   const cookies = new Cookies();
-  return cookies.get(TOKEN);
+  return cookies.get(TOKEN, { path: '/' });
 };
