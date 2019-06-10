1023a266360b6c31e95443d22a3e25725872a950
Sergey P <pozhilov@gmail.com>
2019-06-10 14:35:53 +0300

    Simple Feed page blade template

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -1,18 +1,43 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Feed</title>
-</head>
-<body>
-<div class="header">
-    <h1><a href="{{ $permalink }}">{{ $title }}</a></h1>
-</div>
-    @foreach ($items as $item)
-    <div class="item">
-        <h2><a href="{{ $item->get_permalink() }}">{{ $item->get_title() }}</a></h2>
-        <p>{{ $item->get_description() }}</p>
-        <p><small>Posted on {{ $item->get_date('j F Y | g:i a') }}</small></p>
+@extends('layouts.page')
+@section('title', $feed->title . ' - RSS subscription for Kindle')
+@section('page-title', $feed->title)
+
+@section('content')
+
+  <div class="section mt-6">
+    <div class="container">
+      <h2 class="h3 mb-1 font-weight-light">Latest posts</h2>
+      <p class="small mb-5">Note: This is a preview of the feed. Subscribe to it to get updates on your Kindle.</p>
+
+      @foreach ($content as $source)
+        @if (count($source['items']) > 0 )
+          <div class="source">
+            <h5 class="h6 border-bottom pb-1 mb-3">{{ htmlspecialchars_decode($source['title']) }}</h5>
+            <ul class="source__toc list-unstyled">
+              @foreach ($source['items'] as $item)
+                <li>
+                  <a href="{{ $item->get_permalink() }}" target="_blank"
+                     class="font-weight-bold link-dark">{{ htmlspecialchars_decode($item->get_title()) }}</a>
+                  <br>
+                  <small class="text-muted">
+                    {{ Carbon\Carbon::createFromFormat('Y-n-j G:i:s', $item->get_date('Y-n-j G:i:s'))->diffForHumans() }}</small>
+                </li>
+              @endforeach
+            </ul>
+          </div>
+        @endif
+      @endforeach
+
     </div>
-    @endforeach
-</body>
-</html>
+  </div>
+
+  <div class="section section-cta text-center mb-7">
+    <div class="container">
+      <p class="mt-4">
+        <a class="btn btn-lg-cta" href="/app/">Sign in or Register to subscribe</a>
+      </p>
+    </div>
+  </div>
+
+
+@endsection
