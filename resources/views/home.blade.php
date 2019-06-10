12fe6be07ebf29915eadf35ac1fab01da7a76a8e
Sergey P <pozhilov@gmail.com>
2019-06-10 14:34:04 +0300

    Updated home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
-@section('title', 'Read RSS feeds while offline, in the sun - ReaderFeeder')
+@section('title', 'Read RSS feeds while offline, in the sun')
 
 @section('content')
 
@@ -13,7 +13,7 @@
             <img width="65%" src="/assets/images/hero.svg" alt="" class="mt-3">
           </span>
           </h1>
-          <h2 class="font-family-base h5 font-weight-bold pr-xl-8">
+          <h2 class="h5 font-weight-bold pr-xl-8">
             Follow your favorite blogs, sites &amp; news portals —
             read them all from the comfort of your Kindle Paperwhite!
             <span class="small d-block mt-3 mb-4">
@@ -34,12 +34,12 @@
 
   <div class="section features">
     <div class="container">
-      <h2 id="features" class="text-center mb-0">Features</h2>
+      <h2 id="features" class="text-serif text-center mb-0">Features</h2>
       <p class="mb-5 mb-lg-7 text-muted text-center">So, why ReaderFeeder is awesome?</p>
       <div class="row">
         <div class="col-md-6 col-lg-5 offset-lg-1">
           <img src="/assets/images/brain.svg" alt="" width="64">
-          <h3 class="font-family-base h6 mt-4 mb-3">Distraction-free reading</h3>
+          <h3 class="h6 mt-4 mb-3">Distraction-free reading</h3>
           <p>
             With Kindle, it's easy to concentrate on the reading, not on the countless
             notifications, ads and other bells and whistles you are getting every second
@@ -48,7 +48,7 @@
         </div>
         <div class="col-md-6 col-lg-5">
           <img src="/assets/images/hand.svg" alt="" width="64">
-          <h3 class="font-family-base h6 mt-4 mb-3">Helps your eyes relax</h3>
+          <h3 class="h6 mt-4 mb-3">Helps your eyes relax</h3>
           <p>
             Spend entire days in front of your PC screen?
             Then you should definitely consider making ReaderFeeder your default RSS
@@ -60,7 +60,7 @@
       <div class="row mt-5">
         <div class="col-md-6 col-lg-5 offset-lg-1">
           <img src="/assets/images/fulltext.svg" alt="" width="64">
-          <h3 class="font-family-base h6 mt-4 mb-3">Full-text content</h3>
+          <h3 class="h6 mt-4 mb-3">Full-text content</h3>
           <p>
             There are feeds that only provide summary posts with a link to the full
             one. That would force you to use the web browser on Kindle,
@@ -70,7 +70,7 @@
         </div>
         <div class="col-md-6 col-lg-5">
           <img src="/assets/images/wifi.svg" alt="" width="64">
-          <h3 class="font-family-base h6 mt-4 mb-3">Wireless delivery</h3>
+          <h3 class="h6 mt-4 mb-3">Wireless delivery</h3>
           <p>
             To receive a set of fresh news and updates from ReaderFeeder, you don't even need
             to connect your Kindle to computer using a wire. Just make sure its wifi is turned
@@ -83,7 +83,7 @@
 
   <div class="section mt-5 mt-md-8 pricing">
     <div class="container">
-      <h2 id="pricing" class="text-center mb-5 mb-lg-7">Pricing</h2>
+      <h2 id="pricing" class="text-serif text-center mb-5 mb-lg-7">Pricing</h2>
 
       <p class="text-center">
         <img src="/assets/images/price.png" alt="only $5/mo" class="img-fluid" style="margin-left: -10px;">
@@ -104,18 +104,21 @@
         Unlimited support.
       </p>
 
-      <p class="text-center">
-        And of course, you can try Readerfeeder for free,<br>
-        for an unlimited period of time, with a single RSS subscription.
-      </p>
     </div>
   </div>
 
   <div class="section section-cta text-center mb-6">
     <div class="container">
-      <h2 class="section__title">Get started with ReaderFeeder today</h2>
-      <p class="mt-4"><a class="btn btn-lg btn-lg-cta" href="/app/">Try it free</a></p>
-      <small class="text-muted">No credit card required</small>
+      <h2 class="text-serif">Get started with ReaderFeeder today</h2>
+      <p class="text-center">
+        And of course, you can try Readerfeeder for free,<br>
+        for an unlimited period of time, with a single RSS subscription.
+      </p>
+      <p class="mt-4">
+        <a class="btn btn-lg btn-lg-cta" href="/app/">Try it free</a>
+        <br>
+        <small class="text-muted">No credit card required</small>
+      </p>
     </div>
   </div>
 
