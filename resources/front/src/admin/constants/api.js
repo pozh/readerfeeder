8e2e2f19e6fddb08168e72dcd72822dabf5debb6
Sergey P <pozhilov@gmail.com>
2019-06-24 16:46:37 +0300

    remove /admin, reorganize folders structure

--- resources/front/src/admin/constants/api.js
+++
@@ -1,3 +0,0 @@
-export const API_LOGIN = API_ROOT + '/auth/login';
-export const API_USER = API_ROOT + '/user';
-export const TOKEN = 'token';
--- resources/front/src/admin/constants/message.js
+++
@@ -1,6 +0,0 @@
-export const DELETE_CONFIRM_MESSAGE = 'Are you sure you want to delete it?';
-export const NOT_AUTHORIZED = 'You are not authorized to do this. Please login and try again.';
-export const INVALID_LOGIN_DATA = 'Invalid username or password.';
-export const SETTINGS_MISSING_VALUE = 'All fields are required.';
-export const SETTINGS_SAVED = 'Settings saved.';
-export const ERROR = 'Error.';
