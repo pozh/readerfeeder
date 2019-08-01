868f5096e0941911d33621e58d23ef230e1153f3
Sergey P <pozhilov@gmail.com>
2019-08-01 13:49:00 +0300

    Facebook and Google login buttons

--- resources/front/src/scenes/Auth/components/SocialAuth.js
+++ resources/front/src/scenes/Auth/components/SocialAuth.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
+import GoogleLogin from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
+import * as authAction from 'actions/authAction';
 import './styles.scss';
 
 class SocialAuth extends Component {
-
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(provider) {
-    window.location.assign(`${SOCIAL_ROOT}/${provider}`);
+  responseFacebook(response) {
+    console.log(response);
+  }
+
+  responseGoogle(response) {
+    console.log(response);
   }
 
   render() {
+    const FbIcon = () => (<img src={require('assets/images/ico_facebook.png')} alt="" />);
     return (
       <span className="social-auth">
         <span className="or">or</span>
-        <button onClick={() => { this.handleClick('facebook'); }} type="button" className="btn btn-circle btn-facebook">
-          <img src={require('assets/images/ico_facebook.png')} alt="" />
-        </button>
-        <button onClick={() => { this.handleClick('google'); }} type="button" className="btn btn-circle btn-google">
-          <img src={require('assets/images/ico_google.png')} alt="" />
-        </button>
+        <FacebookLogin
+          appId="2340051056323088"
+          icon={<FbIcon />}
+          cssClass="btn btn-circle btn-facebook mr-1"
+          textButton=""
+          fields="name,email,picture"
+          callback={this.responseFacebook}
+        />
+        <GoogleLogin
+          clientId="218868329575-r8ou1d3et0a2k5rrmairq691oqir8tf9.apps.googleusercontent.com"
+          render={renderProps => (
+            <button
+              type="button"
+              className="btn btn-circle btn-google"
+              onClick={renderProps.onClick}
+              disabled={renderProps.disabled}
+            >
+              <img src={require('assets/images/ico_google.png')} alt="" />
+            </button>
+          )}
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+        />
       </span>
     );
   }
