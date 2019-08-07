0debe66b4151e1f059babf7831572bcaf7d0d39e
Sergey P <pozhilov@gmail.com>
2019-08-07 10:30:17 +0300

    Settings - TextInput, Billing plan (basic) layout

--- resources/front/src/scenes/User/Settings.js
+++ resources/front/src/scenes/User/Settings.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import _assign from 'lodash/assign';
 import * as authAction from 'actions/authAction';
+import TextInput from 'components/ui/TextInput';
 import { Header, PageCaption } from 'components/index';
 
 
@@ -11,8 +12,6 @@ class Settings extends Component {
     super(props);
     const settings = JSON.parse(props.user.settings);
     this.state = {
-      errors: {},
-      redirect: false,
       settings: {
         id: props.user.id,
         first_name: props.user.first_name,
@@ -20,7 +19,16 @@ class Settings extends Component {
       }
     };
 
+    this.handleChange = this.handleChange.bind(this);
     this.processForm = this.processForm.bind(this);
+  }
+
+  handleChange(event) {
+    // const { name, value } = event.target;
+    // this.setState(prevState => {
+    //   const user = { ...prevState.user, [name]: value };
+    //   return { ...prevState, user };
+    // });
   }
 
   processForm(event) {
@@ -30,7 +38,7 @@ class Settings extends Component {
 
   render() {
     const { user } = this.props;
-    const settings = JSON.parse(user.settings);
+    const settings = JSON.parse(user.settings) || {};
     return (
       <div>
         <Header />
@@ -40,34 +48,30 @@ class Settings extends Component {
             <div className="row justify-content-center">
               <div className="col-md-6">
                 <form method="post" id="settingsform" onSubmit={this.processForm}>
-                  <div className="form-group">
-                    <label htmlFor="name">Your Name</label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      name="name"
-                      id="name"
-                      onChange={(val) => {
-                        this.state.settings.first_name = val;
-                      }}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="kindle_email">Kindle Email <sup>*</sup></label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      name="kindle_email"
-                      id="kindle_email"
-                      placeholder="Your Kindle's e-mail"
-                      onChange={(val) => {
-                        this.state.settings.kindle_email = val;
-                      }}
-                    />
-                  </div>
+                  <TextInput
+                    label="Your Name"
+                    name="fName"
+                    required
+                    value={user.first_name}
+                    onChange={this.handleChange}
+                  />
+                  <TextInput
+                    label="Kindle Email"
+                    name="kindle_email"
+                    required
+                    value={settings.kindle_email}
+                    onChange={this.handleChange}
+                  />
                   <div className="form-group text-center">
                     <br />
-                    <button className="btn btn-lg btn-primary">Save settings</button>
+                    <button type="button" className="btn btn-lg btn-primary">Save settings</button>
+                  </div>
+                  <div className="form-group mt-6 text-center">
+                    <p>
+                      <strong>Membership: </strong>
+                      Free, since March 2019
+                    </p>
+                    <button type="button" className="btn btn-primary">Upgrade</button>
                   </div>
                 </form>
               </div>
@@ -83,6 +87,7 @@ class Settings extends Component {
 function mapStateToProps(state) {
   return {
     user: state.auth.user,
+    usermeta: state.auth.usermeta,
     apiState: state.api
   };
 }
